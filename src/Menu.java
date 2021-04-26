573bc4377b88b448ffc335361ed65407b6879846
Vincent Nguyen <61442199+vincenthihi@users.noreply.github.com>
2021-04-25 21:28:08 -0700

    Update Menu.java
    Abstract Class for Food Items

--- src/Menu.java
+++ src/Menu.java
@@ -1,35 +1,29 @@
-public class Menu {
 
-    //Menu Price
-    private final double BURGER_PRICE = 10.00;
-    private final double PIZZA_PRICE = 6.00;
-    private final double MILKSHAKE_PRICE = 2.00;
-    private final double ALFREDO_PRICE = 15.00;
-    private final double HOTDOG_PRICE = 2.50;
-
-    //Menu Items
-    private final String BURGER = "1";
-    private final String PIZZA = "2";
-    private final String MILKSHAKE= "3";
-    private final String ALFREDO = "4";
-    private final String HOTDOG = "5";
+public abstract class Menu {
+    private String name;
+    private double price;
 
 
-    public static void printMenu() {
+    public Menu(String foodName, double foodPrice) {
+        this.name= name;
+        this.price = price;
 
-    System.out.println("===================================");
-    System.out.println("      Menu      ");
-    System.out.println("----------------");
-    System.out.println("1. Burger        10.00$|" + "Qty: ");
-    System.out.println("2. Pizza         6.00$|" + "Qty: ");
-    System.out.println("3. Milkshake     2.00$|" + "Qty: ");
-    System.out.println("4. Alfredo       15.00$|" + "Qty: ");
-    System.out.println("5. Hotdog         2.50$|" + "Qty: ");
-    System.out.println("===================================");
-    System.out.println("Please Select Item number");
-}
+    }
 
-public String getMenuItem()
+    public String getFoodName() {
+        return name;
+    }
 
+    public void setFoodName(String foodName) {
+        this.name = foodName;
+    }
+
+    public double getFoodPrice() {
+        return price;
+    }
+
+    public void setFoodPrice(double foodPrice) {
+        this.price = foodPrice;
+    }
 }
 
